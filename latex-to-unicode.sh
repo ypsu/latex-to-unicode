b814dc627e2cb11e7a6b60e47f967141ef10fec8
Balazs Kezes <rlblaster@gmail.com>
2011-06-09 23:02:26 +0200

    Copy result into more clipboards to work more reliably.

--- latex-to-unicode.sh
+++ latex-to-unicode.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 cd "$(dirname $(echo "$0"))"
-./gui.py | tr -d '\n' | xclip
+text=$(./gui.py)
+echo -n "$text" | xclip
+echo -n "$text" | xclip -selection clipboard
 xdotool key shift+Insert
