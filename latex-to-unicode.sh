d40591b642f6f128de8aec368d21e1bfa4e66ad1
Balazs Kezes <rlblaster@gmail.com>
2011-10-29 19:56:59 +0200

    Don't paste when cancelled or no output

--- latex-to-unicode.sh
+++ latex-to-unicode.sh
@@ -2,6 +2,9 @@
 
 cd "$(dirname $(echo "$0"))"
 text=$(./gui.py)
+if test -z "$text"; then
+	exit
+fi
 echo -n "$text" | xclip
 echo -n "$text" | xclip -selection clipboard
 xdotool key shift+Insert
